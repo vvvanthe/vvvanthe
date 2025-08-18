b88385b0855b8d5fc7302278ad8dd9bbe0cf3a23
vvvanthe <the20091@gmail.com>
2025-08-18 16:00:17 +0900

    add slides

--- packages/js/main.js
+++ packages/js/main.js
@@ -391,86 +391,85 @@ pjModalCloses.forEach((closeBtn) => {
 // });
 
 // IMAGE SLIDES & CIRCLES ARRAYS, & COUNTER
-var imageSlides = document.getElementsByClassName('imageSlides');
-var circles = document.getElementsByClassName('circle');
-var leftArrow = document.getElementById('leftArrow');
-var rightArrow = document.getElementById('rightArrow');
-var counter = 0;
-// Captions array
-const captions = [
-  "SIGGRAPH ASIA 2023, Sydney, Australia",
-  "Automotive World 2024, Tokyo, Japan",
-  "Speech in IPUT 2024, Tokyo, Japan",
-  "ICIP 2024, Abu Dhabi, UAE",
-  "Thesis Defense 2025, Seoul, South Korea"
-];// Caption element
-const caption = document.getElementById('slideCaptions');
+document.addEventListener('DOMContentLoaded', function() {
+  // IMAGE SLIDES & CIRCLES ARRAYS, & COUNTER
+  var imageSlides = document.getElementsByClassName('imageSlides');
+  var circles = document.getElementsByClassName('circle');
+  var leftArrow = document.getElementById('leftArrow');
+  var rightArrow = document.getElementById('rightArrow');
+  var counter = 0;
 
-// HIDE ALL IMAGES FUNCTION
-function hideImages() {
-  for (var i = 0; i < imageSlides.length; i++) {
-    imageSlides[i].classList.remove('visible');
-  }
-}
+  // Captions array
+  const captions = [
+    "SIGGRAPH ASIA 2023, Sydney, Australia",
+    "Automotive World 2024, Tokyo, Japan",
+    "Speech in IPUT 2024, Tokyo, Japan",
+    "ICIP 2024, Abu Dhabi, UAE",
+    "Thesis Defense 2025, Seoul, South Korea"
+  ];
 
-// REMOVE ALL DOTS FUNCTION
-function removeDots() {
-  for (var i = 0; i < imageSlides.length; i++) {
-    circles[i].classList.remove('dot');
-  }
-}
+  // Caption element
+  const caption = document.getElementById('slideCaptions');
 
-// SINGLE IMAGE LOOP/CIRCLES FUNCTION
-function imageLoop() {
-  var currentImage = imageSlides[counter];
-  var currentDot = circles[counter];
-  currentImage.classList.add('visible');
-  removeDots();
-  currentDot.classList.add('dot');
-
-  // update caption based on current index
-  caption.innerText = captions[counter];
-
-  counter++;
-}
-
-// LEFT & RIGHT ARROW FUNCTION & CLICK EVENT LISTENERS
-function arrowClick(e) {
-  var target = e.target;
-  clearInterval(imageSlideshowInterval);
-  hideImages();
-  removeDots();
-
-  if (target == leftArrow) {
-    if (counter == 1) {
-      counter = (imageSlides.length - 1);
-    } else {
-      counter -= 2;
+  // HIDE ALL IMAGES FUNCTION
+  function hideImages() {
+    for (var i = 0; i < imageSlides.length; i++) {
+      imageSlides[i].classList.remove('visible');
     }
-  } else if (target == rightArrow) {
-    if (counter == imageSlides.length) {
+  }
+
+  // REMOVE ALL DOTS FUNCTION
+  function removeDots() {
+    for (var i = 0; i < circles.length; i++) {
+      circles[i].classList.remove('dot');
+    }
+  }
+
+  // SINGLE IMAGE LOOP/CIRCLES FUNCTION
+  function imageLoop() {
+    hideImages();
+    removeDots();
+
+    var currentImage = imageSlides[counter];
+    var currentDot = circles[counter];
+    currentImage.classList.add('visible');
+    currentDot.classList.add('dot');
+
+    // update caption
+    caption.innerText = captions[counter];
+
+    counter++;
+    if (counter >= imageSlides.length) {
       counter = 0;
     }
   }
 
-  imageLoop();
-  imageSlideshowInterval = setInterval(slideshow, 10000);
-}
+  // LEFT & RIGHT ARROW FUNCTION & CLICK EVENT LISTENERS
+  function arrowClick(e) {
+    clearInterval(imageSlideshowInterval);
 
-leftArrow.addEventListener('click', arrowClick);
-rightArrow.addEventListener('click', arrowClick);
-
-// IMAGE SLIDE FUNCTION
-function slideshow() {
-  if (counter < imageSlides.length) {
+    if (e.target == leftArrow) {
+      counter -= 2;
+      if (counter < 0) counter = imageSlides.length - 1;
+    }
     imageLoop();
-  } else {
-    counter = 0;
-    hideImages();
+
+    imageSlideshowInterval = setInterval(slideshow, 10000);
+  }
+
+  leftArrow.addEventListener('click', arrowClick);
+  rightArrow.addEventListener('click', function(e) {
+    clearInterval(imageSlideshowInterval);
+    imageLoop();
+    imageSlideshowInterval = setInterval(slideshow, 10000);
+  });
+
+  // IMAGE SLIDE FUNCTION
+  function slideshow() {
     imageLoop();
   }
-}
 
-// SHOW FIRST IMAGE, & THEN SET & CALL SLIDE INTERVAL
-setTimeout(slideshow, 1000);
-var imageSlideshowInterval = setInterval(slideshow, 10000);
+  // INITIAL CALL
+  slideshow();
+  var imageSlideshowInterval = setInterval(slideshow, 10000);
+});
